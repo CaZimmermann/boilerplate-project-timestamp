579c96ea49e5d804a61dd203cca7598757d9d627
CaZimmermann <103119813+CaZimmermann@users.noreply.github.com>
2025-01-21 12:29:24 +0100

    feat: complete project

--- index.js
+++ index.js
@@ -20,8 +20,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+ 
+
+  if (!dateParam) {
+    const now = new Date();
+    
+    return res.json({
+      unix: now.getTime(),
+      utc: now.toUTCString(),
+    });
+}
+
+const date = isNaN(dateParam) ? new Date(dateParam) : new Date(Number(dateParam));
+
+
+if (date.toString() === "Invalid Date") {
+  return res.json({ error: "Invalid Date" });
+}
+
+res.json({
+  unix: date.getTime(),
+  utc: date.toUTCString(),
+});
 });
 
 
